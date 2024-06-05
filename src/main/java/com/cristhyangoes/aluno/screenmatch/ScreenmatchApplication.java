88b8d787c8945bd4835cb99a1d5c0d4e66bbd0e3
cristhyangoes <cgoesfelipe@gmail.com>
2024-06-05 16:45:21 -0300

    inicio do projeto Spring

---
+++ src/main/java/com/cristhyangoes/aluno/screenmatch/ScreenmatchApplication.java
@@ -0,0 +1,27 @@
+package com.cristhyangoes.aluno.screenmatch;
+
+import com.cristhyangoes.aluno.screenmatch.model.DadosSerie;
+import com.cristhyangoes.aluno.screenmatch.service.ConsumoApi;
+import com.cristhyangoes.aluno.screenmatch.service.ConverteDados;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScreenmatchApplication implements CommandLineRunner {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScreenmatchApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		var consumoApi = new ConsumoApi();
+		var json = consumoApi.obterDados("https://www.omdbapi.com/?t=game+of+thrones&apikey=8cfb1a6d");
+		System.out.println(json);
+
+		ConverteDados conversor = new ConverteDados();
+		DadosSerie dados = conversor.obterDados(json, DadosSerie.class);
+		System.out.println(dados);
+	}
+}
---
+++ src/main/java/com/cristhyangoes/aluno/screenmatch/service/ConsumoApi.java
@@ -0,0 +1,29 @@
+package com.cristhyangoes.aluno.screenmatch.service;
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+
+public class ConsumoApi {
+
+    public String obterDados(String endereco){
+        HttpClient client = HttpClient.newHttpClient();
+        HttpRequest requisicao = HttpRequest.newBuilder()
+                .uri(URI.create(endereco))
+                .build();
+
+        HttpResponse<String> resposta = null;
+        try{
+            resposta = client.send(requisicao, HttpResponse.BodyHandlers.ofString());
+        }catch(IOException e){
+            throw new RuntimeException(e);
+        }catch(InterruptedException e){
+            throw new RuntimeException(e);
+        }
+
+        String json = resposta.body();
+        return json;
+    }
+}
